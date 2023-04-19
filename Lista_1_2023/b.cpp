fb37d5c81987e26ca58890210d5ebb45d46ac474
diogjunior100 <diogjunior10071@gmail.com>
2023-04-19 12:17:43 -0300

    Adiciona resolucao da b.cpp

--- Lista_1_2023/b.cpp
+++ Lista_1_2023/b.cpp
@@ -1,9 +1,51 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-
 int main(){
+    long long x = 0;
+    long long n, k;
+
+    cin >> n >> k;
+
+    vector<long long> as(n);
+
+    for (long long i = 0; i < n; ++i)
+    {
+        cin >> as[i];
+    }
+
+    for (int i = 60; i >= 0; --i)
+    {
+        auto mask = (1LL << i);
+        int z = 0, o = 0;
+
+        for (auto a: as)
+        {
+            //a & mask ? ++o : ++z;
+            if (a & mask)
+            {
+                ++o;
+            }
+            else{
+                ++z;
+            }
+            
+        }
+        if (z > o && (x | mask) <= k)
+        {
+            x |= mask;
+        }
+    }
     
+    long long sum = 0;
+    for (auto num : as) {
+        long long result = x ^ num;
+        sum = sum + result;
+    }
+    
+    cout << sum << endl;
+
     return 0;
 }
